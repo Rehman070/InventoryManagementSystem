7d251dbe4aa2f06bcd35afd0c463810e68b05b42
Abdur Rehman <abdurrehman5732@gmail.com>
2025-04-10 19:30:04 +0500

    feat: add navbar, product form, product list, purchase form, sales list components
    - Implemented NavbarComponent with a simple template.
    - Created ProductFormComponent with a placeholder template.
    - Added ProductListComponent with a basic structure.
    - Developed PurchaseFormComponent with initial setup.
    - Introduced SalesListComponent with a simple display.
    - Updated app.component.ts to include NgbModule for Bootstrap support.

---
+++ InventorySystem.Client/src/app/components/purchases-list/purchases-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchases-list',
+  imports: [],
+  templateUrl: './purchases-list.component.html',
+  styleUrl: './purchases-list.component.scss'
+})
+export class PurchasesListComponent {
+
+}
